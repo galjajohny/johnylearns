529e96b7f137c7a9de4de0e3602aa7fbd9759b2c
galja_johny <galga.johny@gmail.com>
2021-11-07 04:36:59 +0300

    rename class

--- src/test/java/galja_johny/DragAndDropTest.java
+++ src/test/java/galja_johny/DragAndDropTest.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class DragandDropTest {
+public class DragAndDropTest {
 
     @BeforeEach
     public void setup() {
